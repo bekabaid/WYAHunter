d0d0e9953e2d5daa3ae9872d31f4ef99ea2db5e2
castilloe13 <erick.castillo33@myhunter.cuny.edu>
2016-10-05 20:50:59 -0400

    Added login and create controller.
    Can now create users
    Password encryption is present
    
    Can now login and start sessions.
    
    Will be able to redirect to login page using:
    
     before_action :require_user,
    
     Once server is loaded with info.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'create'  => 'users#new'
+  resources :users
+
+  get 'login' => 'sessions#new.html.erb'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  # For details on the D
+  # available within this file, see http://guides.rubyonrails.org/routing.html
 end
