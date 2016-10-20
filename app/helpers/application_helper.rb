2be2442150086eb0377768bde35f21f2c52bc67f
kevin <kevinsanchez1989@gmail.com>
2016-10-20 11:55:38 -0400

    David made it more multiple purpose through MY laptop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,6 @@ module ApplicationHelper
   def user_info
     user_id = session[:user_id]
     user = User.find(user_id)
-    if user != nil
-      return user.my_name
-    end
-    return "doesn't work"
+    return user
   end
 end
