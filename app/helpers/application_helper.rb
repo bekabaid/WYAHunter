e2d953e72410085d4820d5b4e60c8ce952d87c13
kevin <kevinsanchez1989@gmail.com>
2016-10-20 11:52:17 -0400

    dashboard retrieves name from table by using helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def user_info
+    user_id = session[:user_id]
+    user = User.find(user_id)
+    if user != nil
+      return user.my_name
+    end
+    return "doesn't work"
+  end
 end
