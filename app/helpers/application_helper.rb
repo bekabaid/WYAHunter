610330aa6b87f1268cd42a496b1295514c53edb1
kevin <kevinsanchez1989@gmail.com>
2016-10-20 12:04:35 -0400

    added a second 'user_name' function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     user = User.find(user_id)
     return user
   end
+
+  def user_name
+    user = user_info
+    if user != nil
+      return user.my_name
+    end
+    return " "
+    end
+
 end
