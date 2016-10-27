ad46cfed590c25e5f5b928f69854c40443bb3d73
bekabaid <bekabaid@gmail.com>
2016-10-27 11:13:43 -0400

    Merge branch 'master' of https://github.com/candidcold/WYAHunter
    # Conflicts:
    #	Gemfile.lock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,6 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
-  def show
-    @user = User.find(params[:id])
-    redirect_to root_path unless @user == current_user
-  end
-
   helper_method :require_user
 
   private
