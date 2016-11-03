bf8d11d511c6b2184cd63006805f2fd4b7874a70
castilloe13 <erick.castillo33@myhunter.cuny.edu>
2016-11-03 10:33:05 -0400

    Email Uniqueness online!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if (@user.email == User.find_by_email(@user.email).email)
+    flash.now[:danger] = 'email already linked to account'
+    render 'new'
+    else @user.save
       session[:user_id] = user.id
       redirect_to '/'
-    else
-      redirect_to '/create'
     end
 
   end
