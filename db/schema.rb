2b9971bf27918434aaaaead3d0500a0ca679c178
kevin <kevinsanchez1989@gmail.com>
2016-11-23 16:31:55 -0500

    Swap to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161103144747) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "resources", force: :cascade do |t|
     t.string   "location"
     t.integer  "privilege"
