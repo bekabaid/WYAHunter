939b6a9dbbc34fe2d5fa2a785d6f86d2c548f395
David Morant <dmorant95@gmail.com>
2016-11-28 12:12:51 -0500

    Using sqlite in testing, and postgres in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,5 +51,5 @@
                               available: 42,
                               resource_type: 'laptops')
 
-User.create(my_name: 'David', email: 'david@david.com',
+User.create(my_name: 'Beka', email: 'beka@beka.com',
             password_digest:'$2a$10$/wUgVZwYw2W4mWuqwVtoqOir3UMYUN12JrumQukNJjSCWLZRe/paa', privilege: -1)
