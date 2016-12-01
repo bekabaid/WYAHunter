609488ade016cfcf6ea60cdc1c54edf9d280ee05
Erick Castillo <Erick.Castillo33@myhunter.cuny.edu>
2016-12-01 12:25:01 -0500

    more mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'https://wyahunter.herokuapp.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = ''
+    mail(to: @user.email, subject: 'you made it breh')
+  end
 end
