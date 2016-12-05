f20c90197ce9663e70c1677d3704cd728bbca0b4
bekabaid <bekabaid@gmail.com>
2016-12-05 10:10:18 -0500

    Update admin page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new.html.erb'
   get 'dashboard' => 'sessions#dashboard.html.erb'
   get 'admin_dashboard' => 'sessions#admin_dashboard.html.erb'
+  get 'admin_dashboard_newResource' => 'sessions#admin_dashboard_newResource.html.erb'
+  get 'admin_dashboard_createUser' => 'sessions#admin_dashboard_createUser.html.erb'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
   # available within this file, see http://guides.rubyonrails.org/routing.html
