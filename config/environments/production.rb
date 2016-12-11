f830c24b95395566d22fa08a2c042cae3997a676
bekabaid <bekabaid@gmail.com>
2016-12-11 13:16:20 -0500

    possible fix for heroku views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.assets.precompile += ['static_pages.css']
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
